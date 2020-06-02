b0ebd2571af14439e02414699a5c6f70c2766bf7
nibir <biplob.asanibir@gmail.com>
2020-06-02 18:12:47 +0600

    add Payment and MemberShipPlan Componets

---
+++ src/components/MemberShipPlan/MemberShipPlan.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MemberShipPlan = () => {
+    return (
+        <div>
+            Price plan goes here
+        </div>
+    );
+};
+
+export default MemberShipPlan;
---
+++ src/components/Payments/Payments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Payments = () => {
+    return (
+        <div>
+            Payment Components goes there
+            1.Stripe
+            2.Paypal
+        </div>
+    );
+};
+
+export default Payments;
