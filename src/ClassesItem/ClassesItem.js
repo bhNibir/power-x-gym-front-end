6758322c9cf2b2a7ef81eac0a729630b758e6e42
nibir <biplob.asanibir@gmail.com>
2020-06-03 16:17:43 +0600

    footer design completed

--- src/ClassesItem/ClassesItem.js
+++ src/ClassesItem/ClassesItem.js
@@ -8,12 +8,6 @@ import { FaArrowRight } from 'react-icons/fa';
 const ClassesItem = ({item}) => {
     return (
         
-            // {/* <section className="">
-            //     <div class="col-12 col-sm-6 col-lg-3 position-relative">
-            //     <a href="https://google.com" class="d-block"><img src="https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="w-100"/></a>
-            //     <a href="https://google.com" class="btn btn-primary classes-btn">Button</a>
-            //     </div>
-            // </section> */}
            <Col xs={6} md={4}>
                <div className="position-relative m-3 text-center">
                 <Image
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { GrYoutube, GrFacebook, GrInstagram, GrTwitter } from 'react-icons/gr';
+import { FaWhatsapp } from 'react-icons/fa';
 
 const Footer = () => {
     return (
@@ -9,40 +11,46 @@ const Footer = () => {
             <div className="pt-4 my-md-5 pt-md-5">
             <div className="row">
                 <div className="col-12 col-md">
-                <h2  className="pt-2" >POWER <span className="text-primary font-weight-bolder">X</span> </h2>
+                    <h2  className="pt-2" >POWER <span className="text-primary font-weight-bolder">X</span> </h2>
                 </div>
                 <div className="col-6 col-md">
-                    <h5>Features</h5>
+                    <h5>Need Help?</h5>
                     <ul className="list-unstyled text-small">
-                    <li><a className="text-white"  href="#">Cool stuff</a></li>
-                    <li><a className="text-white"  href="#">Random feature</a></li>
-                    <li><a className="text-white"  href="#">Team feature</a></li>
-                    <li><a className="text-white"  href="#">Stuff for developers</a></li>
-                    <li><a className="text-white"  href="#">Another one</a></li>
-                    <li><a className="text-white"  href="#">Last time</a></li>
+                        <li><a href>Help Center</a></li>
+                        <li><a href>Email Support</a></li>
+                        <li><a href>Live Chat</a></li>
+                        <li><a href>Gift Certificates</a></li>
+                        <li><a href>Send Us feedback</a></li>
                     </ul>
                 </div>
                 <div className="col-6 col-md">
-                    <h5>Resources</h5>
+                    <h5>Digital Resources</h5>
                     <ul className="list-unstyled text-small">
-                    <li><a className="text-white"  href="#">Resource</a></li>
-                    <li><a className="text-white"  href="#">Resource name</a></li>
-                    <li><a className="text-white"  href="#">Another resource</a></li>
-                    <li><a className="text-white"  href="#">Final resource</a></li>
+                        <li><a href>Articles</a></li>
+                        <li><a href>E-Books</a></li>
                     </ul>
                 </div>
                 <div className="col-6 col-md">
-                    <h5>About</h5>
-                    <ul className="list-unstyled text-small">
-                    <li><a className="text-white"  href="#">Team</a></li>
-                    <li><a className="text-white"  href="#">Locations</a></li>
-                    <li><a className="text-white"  href="#">Privacy</a></li>
-                    <li><a className="text-white"  href="#">Terms</a></li>
+                    <h5>Connect with Us</h5>
+                    <ul className="list-unstyled list-inline">
+                        <li className="list-inline-item h3"><a href><GrYoutube/></a></li>
+                        <li className="list-inline-item h3"><a href><GrFacebook/></a></li>
+                        <li className="list-inline-item h3"><a href><GrInstagram/></a></li>
+                        <li className="list-inline-item h3"><a href><GrTwitter/></a></li>                       
+                        <li className="list-inline-item h3"><a href><FaWhatsapp/></a></li>                       
+                        <li className="mt-2"><a href>Forum</a></li>                       
                     </ul>
                 </div>
+                <div className="col-6 col-md">
+                    <h5>Join Our Newsletter</h5>
+                    <p className="text-small">
+                         Get exclusive news features, and updates from The Shredder Weight Loss Academy
+                    </p>
+                </div>
+               
             </div>
            </div>
-           <div className="text-center p-2">
+           <div className="text-center p-2 pb-4">
                 <small >© 2020 Power X Gym. All Rights Reserved </small>
             </div>
             
