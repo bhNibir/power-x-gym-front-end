b159e90073e75a8621fef0efc1b1f683ad903029
nibir <biplob.asanibir@gmail.com>
2020-06-03 18:59:16 +0600

    add schedule component

---
+++ src/components/ClassSchedule/ClassSchedule.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
+const ClassSchedule = () => {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <div >
+                            <Image
+                                src="https://i.ibb.co/wMHv90B/classes3.jpg"
+                                width={"auto"}
+                                height={380}
+                                fluid
+
+                            />
+                            <p className="pt-4 p-2 text-muted" style={{lineHeight: 2}}>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dolores cum, quidem hic saepe sunt at maiores odit tenetur, inventore laudantium ad vitae, provident quo id quod consectetur illum iusto. Facere tempore provident consequuntur veniam cumque praesentium a, deleniti enim officiis velit aperiam tempora reprehenderit asperiores qui commodi expedita illo!
+                            </p>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="ml-3 text-center ">
+                            <h1 className="text-uppercase font-weight-bolder"><span className="text-primary">Class</span> Schedule</h1>
+                            <div className="mt-4">
+                                <Row>
+                                    <Col md={6}>
+                                        <div className="shadow p-2 mb-3 bg-white rounded">
+                                            <h4 className="font-weight-bold mb-0">Saturday</h4>
+                                            <small className="text-primary">8:00 AM - 9:00 AM</small>
+                                        </div>
+                                    </Col>
+                                    <Col md={6}>
+                                        <div className="shadow p-2 mb-3 bg-white rounded">
+                                            <h4 className="font-weight-bold mb-0">Monday</h4>
+                                            <small className="text-primary">9:00 AM - 10:00 AM</small>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={6}>
+                                        <div className="shadow p-2 mb-3 bg-white rounded">
+                                            <h4 className="font-weight-bold mb-0">Tuesday</h4>
+                                            <small className="text-primary">8:00 AM - 9:00 AM</small>
+                                        </div>
+                                    </Col>
+                                    <Col md={6}>
+                                        <div className="shadow p-2 mb-3 bg-white rounded">
+                                            <h4 className="font-weight-bold mb-0">Wednesday</h4>
+                                            <small className="text-primary">9:00 AM - 10:00 AM</small>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={6}>
+                                        <div className="shadow p-2 mb-3 bg-white rounded">
+                                            <h4 className="font-weight-bold mb-0">Thursday</h4>
+                                            <small className="text-primary">8:00 AM - 9:00 AM</small>
+                                        </div>
+                                    </Col>
+                                    <Col md={6}>
+                                        <div className="shadow p-2 mb-3 bg-white rounded">
+                                            <h4 className="font-weight-bold mb-0">Friday</h4>
+                                            <small className="text-primary">9:00 AM - 10:00 AM</small>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </div>
+
+                        </div>
+                    </Col>
+                </Row>                
+            </Container>
+        </div>
+    );
+};
+
+export default ClassSchedule;
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import MainBody from '../../components/MainBody/MainBody';
+import { ProgressBar } from 'react-bootstrap'
 
 const Contact = () => {
     return (
         <div>
             <MainBody>
-                    <div id="main" class="text-center center-block container jumbotron">
-                <div id="detail" class="span12">
-                    <h1><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Website Under Construction <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></h1>
+                    <div id="main" className="text-center center-block container jumbotron">
+                <div className="span12">
+                    <h1><i className="fa fa-exclamation-triangle" aria-hidden="true"></i> Website Under Construction <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></h1>
                     <h3>Thank you for your patience while we are renovating</h3>
                     <hr/>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{width: "60%",}}>
-                            60%
-                        </div>
+                    <div >
+                     <ProgressBar striped variant="primary" animated now={60} label={60+"%"} />
                     </div>
                 </div>
             </div>
